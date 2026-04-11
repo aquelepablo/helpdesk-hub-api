3105adb49bcfc5ef327644e4f906b91e35ba410a
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2026-04-11 20:41:15 +0200

    refactor(api): centralize api prefix and route organization

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 
-from app.adapters.http.routers import system_router
+from app.adapters.http.routers import category_router, system_router
 from app.infra.settings.project_metadata import project_metadata
 
-app = FastAPI(
-    title=project_metadata.title,
-    description=project_metadata.description,
-    version=project_metadata.version,
-)
 
-app.include_router(system_router.router)
+def create_app() -> FastAPI:
+
+    API_PREFIX = "/api/v1"
+
+    app = FastAPI(
+        title=project_metadata.title,
+        description=project_metadata.description,
+        version=project_metadata.version,
+    )
+
+    # TODO: Add middleware
+
+    # TODO: Add Exceptions Handler
+
+    app.include_router(system_router.router, prefix=API_PREFIX)
+    app.include_router(category_router.router, prefix=API_PREFIX)
+
+    return app
+
+
+app = create_app()
